b2e855a2dd7d93bd18b2e46a32f61792d6b819dd
Jimmy Romeo Herrera Hernandez <jimmy.herrera@BACNET.CORP.REDBAC.com>
2019-06-11 19:19:26 -0600

    agregando carpeta docs

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(lessMiddleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
-
+//
 app.use('/', indexRouter);
 app.use('/api', apiRouter);
 
